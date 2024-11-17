31c9fe0be2bafea9eb849ab7b13fa68699d6161d
Mayank Chaturvedi <chaturvedimayank70@gmail.com>
2024-11-17 19:29:46 +0530

    BACKEND: Added payment route and controller
    -->FINSIHED<--
    
    ->Added payment routes and controllers
    
    ->In payment controllers, added functions to handle creating payment order and verifying payment
    
    There is no insitation of razorpay instance in server.js file , so i have added it in payment controller file

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -4,5 +4,14 @@ const paymentController = require("./../controllers/payment.controller");
 const router = express.Router();
 
 router.route("/checkout").post(paymentController.paymentCheckout);
+router
+  .route("/payment-verification")
+  .post(paymentController.paymentVerification); //post request mandatory
+
+router.route("/getPaymentKey").get((req, res) => {
+  res.status(200).json({
+    key: "rzp_test_riJ6gnkwQk9Ikn",
+  });
+});
 
 module.exports = router;
