cd78c145cf8c1ba1da6ddb6c595755ef23083f5c
Mayank Chaturvedi <110036015+mayank5515@users.noreply.github.com>
2024-11-30 18:52:50 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/v1": "http://localhost:3000",
-      // "/api/v1": "http://192.168.109.131:3000",
     },
   },
   plugins: [react()],
